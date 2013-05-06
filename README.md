c9a5bd2614eb8323dcbfcf47ec2fcdf02fef5383
James Talmage <james.talmage@jrtechnical.com>
2013-05-06 16:34:32 -0400

    Updated README, more consistent karma.conf

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 #Demonstrate Karma Issue 521
 
 ##Steps to Reproduce
+###Setup Project
 ```
 git clone https://github.com/jamestalmage/karma-issue-521
 cd karma-issue-521
 npm install
 npm test
 ```
+###Edit test-main.js
+You should find that editing anything in the `lib/` or `test/` folders causes karma to rerun tests.
+Editing `test-main.js` is ignored however (which is the issue being raised).
+ 
--- karma.conf.js
+++ karma.conf.js
@@ -12,8 +12,8 @@ files = [
     MOCHA_ADAPTER,
     REQUIRE,
     REQUIRE_ADAPTER,
-    'test-main.js',
-    {pattern:'node_modules/**/*.js',included:false,watched:false,served:true},
+    {pattern: 'test-main.js', included:true, watched:true, served:true},
+    {pattern: 'node_modules/**/*.js',included:false,watched:false,served:true},
     {pattern: 'test/**/*test.js', included: false,watched:true,served:true},
     {pattern: 'lib/**/*.js', included: false,watched:true,served:true}
 ];
